cdc96119b7759eaacc5feef940c6749ad85747d5
sprinter-1995 <aliv321@yandex.ru>
2015-05-06 13:27:35 +0300

    Добавил отображение текста и считывание.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-/**
- * Created by Student on 06.05.2015.
- */
-public class Main {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class  Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String s = "";
+        Scanner in = new Scanner(new File("C:\\Users\\Student\\IdeaProjects\\regexp_lab\\V.txt"));
+        while(in.hasNext())
+            s += in.nextLine() + "\r\n";
+        in.close();
+        System.out.println(s);
+    }
+
+
 }
